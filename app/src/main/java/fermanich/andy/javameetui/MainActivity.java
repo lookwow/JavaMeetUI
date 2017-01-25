551bc85fb3c2d2d16b10d98094a095e73ef8ca47
lookwow <lookwow_@hotmail.com>
2017-01-25 15:16:23 -0600

    a

--- app/src/main/java/fermanich/andy/javameetui/MainActivity.java
+++ app/src/main/java/fermanich/andy/javameetui/MainActivity.java
@@ -23,8 +23,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // Get a reference to all the buttons in our UI
-// Match them up to all our Button objects we declared earlier
+        // Match them up to all our Button objects we declared earlier
         btnAdd = (Button) findViewById(R.id.btnAdd);
         btnTake = (Button) findViewById(R.id.btnTake);
         txtValue = (TextView) findViewById(R.id.txtValue);
@@ -32,10 +33,57 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnShrink = (Button) findViewById(R.id.btnShrink);
         btnReset = (Button) findViewById(R.id.btnReset);
         btnHide = (Button) findViewById(R.id.btnHide);
+
+        // Listen for all the button clicks
+        btnAdd.setOnClickListener(this);
+        btnTake.setOnClickListener(this);
+        txtValue.setOnClickListener(this);
+        btnGrow.setOnClickListener(this);
+        btnShrink.setOnClickListener(this);
+        btnReset.setOnClickListener(this);
+        btnHide.setOnClickListener(this);
     }
 
     @Override
-
-}
     public void onClick(View v) {
+
+        float size;
+
+        switch(v.getId()) {
+            case R.id.btnAdd:
+                value++;
+                txtValue.setText("" + value);
+                break;
+            case R.id.btnTake:
+                value--;
+                txtValue.setText("" + value);
+                break;
+            case R.id.btnReset:
+                value = 0;
+                txtValue.setText("" + value);
+                break;
+            case R.id.btnGrow:
+                size = txtValue.getTextScaleX();
+                txtValue.setTextScaleX(size + 1);
+                break;
+            case R.id.btnShrink:
+                size = txtValue.getTextScaleX();
+                txtValue.setTextScaleX(size - 1);
+                break;
+            case R.id.btnHide:
+                if(txtValue.getVisibility() == View.VISIBLE)
+                {
+                    // Currently visible so hide it
+                    txtValue.setVisibility(View.INVISIBLE);
+                    // Change text on the button
+                    btnHide.setText("SHOW");
+                }else{
+                    // Currently hidden so show it
+                    txtValue.setVisibility(View.VISIBLE);
+                    // Change text on the button
+                    btnHide.setText("HIDE");
+                }
+                break;
+        }
+    }
 }
